406889b9b667b1ae8f7c9d26b8f6297dce0c4699
Michelle Maile <michellemaile29@gmail.com>
2019-01-03 09:39:39 -0800

    assessment changes 1

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,44 +1,44 @@
-var nav = document.getElementById("topNav");
-var main = document.getElementById("main");
-var menu = document.getElementsByClassName("menuitems");
-var close = document.getElementById("closebtn");
-
-//default to measure if/else from
-nav.style.height = "50px";
-main.style.marginTop = "50px";
-for (i = 0; i < menu.length; i++){menu[i].style.marginTop="100px";};
-
-close.addEventListener("click", function(){
-  var menuIcon = close.children;
-  for (i = 0; i < menuIcon.length; i++){
-  menuIcon[i].classList.toggle("active");
-  }   
-});
-
-function navToggle() {	
-	//to close
-	if (nav.style.height <= "180px") {
-	nav.style.height = "50px";
-	main.style.marginTop = "50px";
-	
-    	var i = 0;
-    	for (i = 0; i < menu.length; i++){
-	menu[i].style.opacity="0.0";
-	menu[i].style.marginTop="100px";
-	};
-    	document.body.style.backgroundColor = "rgba(0,0,0,0.0)";
-	
-	} 
-	//to open
-	else if (nav.style.height <= "50px") {
-	nav.style.height = "180px";
-	main.style.marginTop = "180px";
-    	var i = 0;
-    	for (i = 0; i < menu.length; i++){
-	menu[i].style.opacity="1.0";
-	menu[i].style.marginTop="0px";
-	};
-	
-	}
-
-};
+//var nav = document.getElementById("topNav");
+//var main = document.getElementById("main");
+//var menu = document.getElementsByClassName("menuitems");
+//var close = document.getElementById("closebtn");
+//
+////default to measure if/else from
+//nav.style.height = "50px";
+//main.style.marginTop = "50px";
+//for (i = 0; i < menu.length; i++){menu[i].style.marginTop="100px";};
+//
+//close.addEventListener("click", function(){
+//  var menuIcon = close.children;
+//  for (i = 0; i < menuIcon.length; i++){
+//  menuIcon[i].classList.toggle("active");
+//  }   
+//});
+//
+//function navToggle() {	
+//	//to close
+//	if (nav.style.height <= "180px") {
+//	nav.style.height = "50px";
+//	main.style.marginTop = "50px";
+//	
+//    	var i = 0;
+//    	for (i = 0; i < menu.length; i++){
+//	menu[i].style.opacity="0.0";
+//	menu[i].style.marginTop="100px";
+//	};
+//    	document.body.style.backgroundColor = "rgba(0,0,0,0.0)";
+//	
+//	} 
+//	//to open
+//	else if (nav.style.height <= "50px") {
+//	nav.style.height = "180px";
+//	main.style.marginTop = "180px";
+//    	var i = 0;
+//    	for (i = 0; i < menu.length; i++){
+//	menu[i].style.opacity="1.0";
+//	menu[i].style.marginTop="0px";
+//	};
+//	
+//	}
+//
+//};
